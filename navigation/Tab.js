8761993cc262bdc72a0e56c30e9aa8708103d6c8
Erik Kimsey <kimseyerik@gmail.com>
2019-07-12 10:58:03 -0400

    Added styles to tab nav

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -1,6 +1,9 @@
 import { createBottomTabNavigator } from 'react-navigation';
+import color from '../assets/globals/colors';
 import Landing from '../screens/Landing';
 import TextEditor from '../screens/TextEditor';
+console.log(color);
+
 // import screen ...
 // import screen ...
 // import screen ...
@@ -14,20 +17,18 @@ export default (Tab = createBottomTabNavigator(
 	{
 		// initialRouteName: 'Second',
 		tabBarOptions: {
-			// // //' - Label and icon color of the active tab.'
-			// activeTintColor: 'orange',
-			// // //' - Background color of the active tab.'
-			// activeBackgroundColor: 'green',
+			activeTintColor: color.gray,
+			activeBackgroundColor: color.softRed,
 			// // //' - Label and icon color of the inactive tab.'
-			// inactiveTintColor: 'blue',
+			inactiveTintColor: color.softRed,
 			// // //' - Background color of the inactive tab.'
 			// inactiveBackgroundColor: 'yellow',
 			// // //' - Style object for the tab bar.'
 			// style: { borderWidth: 2, borderColor: 'purple' },
 			// // //' - Style object for the tab label.'
-			// labelStyle: { fontWeight: 'bold' },
+			labelStyle: { fontWeight: 'bold' },
 			// // //' - Style object for the tab.'
-			// tabStyle: { paddingBottom: 16 },
+			tabStyle: { paddingBottom: 16 }
 			// // //' - Whether to show label for tab, default is true.'
 			// showLabel: false,
 			// // //' - Whether to show icon for tab, default is true.'
