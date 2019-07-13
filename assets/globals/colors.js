701a5273ca65c24e54e753a5bb6ee8bc76d0ede5
Erik Kimsey <kimseyerik@gmail.com>
2019-07-13 11:24:43 -0400

    landed on a font for Landing header

--- assets/globals/colors.js
+++ assets/globals/colors.js
@@ -1,6 +1,3 @@
-// blue-gray: #556271
-// soft-red: #FF6F73
-
 export default {
 	gray: '#556271',
 	softRed: '#FF6F73'
--- screens/Landing.js
+++ screens/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import * as Font from 'expo-font';
 import color from '../assets/globals/colors';
 import bkgrd from '../assets/handwriting.jpg';
@@ -16,7 +16,13 @@ export default class Landing extends Component {
 			timeless: require('../assets/fonts/Timeless.ttf'),
 			painterz: require('../assets/fonts/Painterz.ttf'),
 			rockness: require('../assets/fonts/Rockness.ttf'),
-			lipstick: require('../assets/fonts/lipstick.ttf')
+			lipstick: require('../assets/fonts/lipstick.ttf'),
+			cubesity: require('../assets/fonts/cubesity.ttf'),
+			fatC: require('../assets/fonts/FatC.ttf'),
+			fatCat: require('../assets/fonts/FatCat.ttf'),
+			slukoni: require('../assets/fonts/Slukoni.otf'),
+			blowing: require('../assets/fonts/BlowingBubble.ttf'),
+			drip: require('../assets/fonts/adrip.ttf')
 		});
 		this.setState({ fontLoaded: true });
 	}
@@ -24,16 +30,18 @@ export default class Landing extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				{this.state.fontLoaded ? (
-					<View>
-						<Text style={[ { fontFamily: 'painterz', color: color.softRed }, styles.header ]}>
-							100 Words
-						</Text>
-						<Text style={[ styles.subHeader, { fontFamily: 'lemon-milk' } ]}>
-							Tell a story in 100 words.
-						</Text>
-					</View>
-				) : null}
+				{/* <ImageBackground source={bkgrd} style={styles.container}> */}
+					{this.state.fontLoaded ? (
+						<View>
+							<Text style={[ { fontFamily: 'drip', color: color.softRed }, styles.header ]}>
+								100 Words
+							</Text>
+							<Text style={[ styles.subHeader, { fontFamily: 'lemon-milk' } ]}>
+								Tell a story in 100 words.
+							</Text>
+						</View>
+					) : null}
+				{/* </ImageBackground> */}
 			</View>
 		);
 	}
