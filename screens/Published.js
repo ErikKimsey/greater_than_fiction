ca3a3d189453520478a6c46667fc223d4efb6786
Erik Kimsey <kimseyerik@gmail.com>
2019-07-21 13:56:10 -0400

    Saving image, but with wrong dimensions

--- screens/Published.js
+++ screens/Published.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Image, CameraRoll } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, CameraRoll, PixelRatio } from 'react-native';
 import { StackActions, NavigationActions } from 'react-navigation';
 import color from '../assets/globals/colors';
-// import { takeSnapshotAsync } from 'expo';
+// import { PixelRatio } from 'expo';
 import { captureRef as takeSnapshotAsync } from 'react-native-view-shot';
 // import { Camera,  } from 'expo';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 import PublishedButton from '../components/buttons/publishedButtons';
+
+const targetPixelCount = 1080; // If you want full HD pictures
+const pixelRatio = PixelRatio.get(); // The pixel ratio of the device
+// pixels * pixelratio = targetPixelCount, so pixels = targetPixelCount / pixelRatio
+console.log(pixelRatio);
+
+const pixels = targetPixelCount / pixelRatio;
 
 export default class Published extends Component {
 	state = {
@@ -17,7 +24,7 @@ export default class Published extends Component {
 	};
 
 	async componentDidMount() {
-		const { status } = await Permissions.askAsync(Permissions.CAMERA);
+		const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
 		console.log({ hasCameraPermission: status === 'granted' });
 
 		this.setState({ hasCameraPermission: status === 'granted' });
@@ -35,7 +42,10 @@ export default class Published extends Component {
 	_saveToCameraRollAsync = async () => {
 		let result = await takeSnapshotAsync(this._container, {
 			format: 'png',
-			result: 'tmpfile'
+			result: 'tmpfile',
+			height: pixels,
+			width: pixels / 1.5,
+			quality: 1
 		});
 
 		let saveResult = await CameraRoll.saveToCameraRoll(result, 'photo');
@@ -73,7 +83,7 @@ export default class Published extends Component {
 			return (
 				<View>
 					{this.state.cameraRollUri && (
-						<Image source={{ uri: this.state.cameraRollUri }} style={{ width: 200, height: 200 }} />
+						<Image source={{ uri: this.state.cameraRollUri }} style={{ width: 200, height: 900 }} />
 					)}
 					<PublishedButton
 						saved={this.state.saved}
