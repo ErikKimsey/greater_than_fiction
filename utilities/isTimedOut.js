2aae1e9ddffb6cd500ffa09f86adf461d12e8187
Erik Kimsey <kimseyerik@gmail.com>
2019-07-29 10:38:55 -0400

    Created isTimedOut file

---
+++ utilities/isTimedOut.js
@@ -0,0 +1,7 @@
+/**
+ * isTimedOut
+ * checks if timer has timed out.
+ * If timed out:
+ *  - check if input fields are empty,
+ *  - provide alert and then navigate to publish with what was written.
+ */
