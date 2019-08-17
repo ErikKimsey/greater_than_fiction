d88da7ca89fed4d8ae194e220e690a7e39043ce7
Erik Kimsey <kimseyerik@gmail.com>
2019-08-17 14:42:42 -0400

    Created promptCheck.js in utilities. In the the file, created TODO and listed high-level steps towards checking for prompt words

---
+++ utilities/promptCheck.js
@@ -0,0 +1,17 @@
+/**
+ * This functionality will determine if the prompt words were used in the user's story.
+ */
+
+/**
+  * TODO:
+  * - 1. get prompt words,
+  * - --- 1.a make sure prompt is array
+  * - 2. get user's story,
+  * - --- 2.a import user story string,
+  * - --- 2.b convert story to array of strings
+  * - 3. find matches b/w individual words of story and prompt list
+  * - 4. if all 3 prompts are matched, return "true"
+  * - 5. if any prompt is missing, return "false"
+  */
+
+const promptCheck = () => {};
